9cf31fed2933ac1c7d2e2daaaf01e05857e98508
arafat-jahan <ajh.official15@gmail.com>
2025-08-23 12:26:49 +0600

    updated

--- lib/screens/register_screen.dart
+++ lib/screens/register_screen.dart
@@ -1,6 +1,10 @@
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:intl_phone_field/intl_phone_field.dart';
+import 'package:users/global/golbal.dart';
+import 'package:users/screens/main_page.dart';
 
 class RegisterScreen extends StatefulWidget {
   const RegisterScreen({super.key});
@@ -21,6 +25,40 @@ class _RegisterScreenState extends State<RegisterScreen> {
 
   // Declare a global key for form
   final _formKey = GlobalKey<FormState>();
+
+void _submit() async{
+  // validated all the form fields
+if(_formKey.currentState!.validate()){
+await firebaseAuth.createUserWithEmailAndPassword(
+    email:emailTextEditingController.text.trim() , password: passwordTextEditingController.text.trim()
+).then((auth) async{
+  currnetUser = auth.user;
+  if(currnetUser != null){
+    Map userMap = {
+      "id": currnetUser!.uid,
+      "name": nameTextEditingController.text.trim(),
+      "email": emailTextEditingController.text.trim(),
+      "address": addressTextEditingController.text.trim(),
+      "phone": phoneTextEditingController.text.trim(),
+    };
+
+    DatabaseReference userRef = FirebaseDatabase.instance.ref().child("users");
+    userRef.child(currnetUser! .uid).set(userMap);
+
+
+  }
+  await Fluttertoast.showToast(msg: "Successfully Registered");
+  Navigator.push(context, MaterialPageRoute(builder: (c) => MainScreen()));
+
+}).catchError((errorMessage){
+  Fluttertoast.showToast(msg: "Error occurred: \n $errorMessage");
+});
+}
+else{
+  Fluttertoast.showToast(msg: "Not all field are valid");
+}
+
+}
 
   @override
   Widget build(BuildContext context) {
@@ -266,6 +304,7 @@ class _RegisterScreenState extends State<RegisterScreen> {
                                 ),
                                 minimumSize: Size(double.infinity, 50),
                               ),
+
                               onPressed: () {
                                 if (_formKey.currentState!.validate()) {
                                   // Proceed with registration
@@ -289,6 +328,35 @@ class _RegisterScreenState extends State<RegisterScreen> {
                                 ),
                               ),
                             ),
+                            SizedBox(height: 20,),
+
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text(
+                                  "Have an account?",
+                                  style: TextStyle(
+                                    color: Colors.grey,
+                                    fontSize: 15,
+                                  ),
+                                ),
+                                SizedBox(width: 5,),
+                                GestureDetector(
+                                  onTap: (){
+
+                                  },
+                                  child: Text(
+                                    'Sign In',
+                                    style: TextStyle(
+                                      fontSize: 15,
+                                      color: darkTheme ? Colors.amber.shade400 : Colors.blue,
+
+                                    ),
+                                  ),
+                                )
+
+                              ],
+                            )
                           ],
                         ),
                       ),
